3ff7690a6762ad8e9c48933225bd8ee2f669d5a6
Alexey Borovkov <lucky.aaz@gmail.com>
2019-06-29 15:44:41 +0300

    startig with section

--- app/Models/Section.php
+++ app/Models/Section.php
@@ -9,10 +9,43 @@
 
 namespace App\Models;
 
+use App\Contracts\Containable;
 use App\Contracts\Scoreable;
 use Illuminate\Support\Collection;
 
-class Section implements Scoreable
+class Section implements Scoreable, Containable
 {
-    
+    /** @var Collection  */
+    private $items;
+    /** @var array $params */
+    private $params;
+    /** @var double $weight */
+    private $weight;
+
+    public function __construct(array $params)
+    {
+        $this->params = $params;
+        $this->items = collect();
+        $this->weight = $params['weight'] ?? 1;
+    }
+
+    public function add(Scoreable $obj)
+    {
+        // TODO: Implement add() method.
+    }
+
+    public function all(): Collection
+    {
+        // TODO: Implement all() method.
+    }
+
+    public function total(): double
+    {
+        // TODO: Implement total() method.
+    }
+
+    public function actual(): double
+    {
+        // TODO: Implement actual() method.
+    }
 }
